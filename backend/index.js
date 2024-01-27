5c96b725d58fbbb4355eae2dc6da6f602eddf0a0
Abhilash <abhikannan200304@gmail.com>
2024-01-27 18:09:07 +0530

    Added index.js of the Backend

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 const express = require("express");
+import mainRouter from "./routes/index.js";
 
+const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1", mainRouter);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
