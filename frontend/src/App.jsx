306ee78a651ad9c6a96ae8cd5e80e5c069bfe877
Abhilash <abhikannan200304@gmail.com>
2024-02-06 09:32:05 +0530

    Intializing the frontend bits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
-
+import {BrowserRouter , Routes , Route} from "react-router-dom"
 function App() {
 
   return (
     <div>
-        Hello world
+        <BrowserRouter>
+        <Routes>
+            <Route path="/signup" element={<SignUp/>}/>
+            <Route path="/signin" element={<SignIn/>}/>
+            <Route path="/dashboard" element={<DashBoard/>}/>
+            <Route path="/send" element={<SendMoney/>}/>
+        </Routes>
+        </BrowserRouter>
     </div>
   )
 }
